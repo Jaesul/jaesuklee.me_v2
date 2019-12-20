cb1efa3f13a30389f5deda3fa466d72f0f2e85a8
Jaesul <jaesul@uw.edu>
2019-12-19 17:19:18 -0800

    finished

--- js/main.js
+++ js/main.js
@@ -5,21 +5,33 @@
 
   let NAV_TIMEOUT;
 
-  function init() {
-    let splashOverlay = document.querySelector('.splash-overlay');
-    splashOverlay.classList.add('set-right');   
+  function init() { 
     let splashCont = document.querySelector('.splash-content');
     let mainContent = document.querySelector('.main-content');
     let main = document.querySelector('main');
-    let abPic = document.querySelector('.picture-overlay')
     let logo = document.querySelector('.logo'); 
     let amContent = document.querySelector('.am-content-container');
     let amPicture = document.querySelector('.am-picture-container');
+    let technologyContainers = document.querySelectorAll('.technology-container');
+    let technologies = document.querySelector('#technologies');
+    let technologiesH2 = technologies.querySelector('h2');
+    let technologyItem = document.querySelectorAll('.technology-item');
+
+
+    technologyItem.forEach(thing => {
+      thing.addEventListener('click', () => {
+        document.getElementById('javascript').classList.remove('slide-top');
+      })
+    });
 
     let options = {
       root: null,
       rootMargin: "0px 0px -100% 0px",
       threshold: 0
+    }
+
+    let options2 = {
+      rootMargin: '0px 0px -17% 0px'
     }
 
     let previousY = 0;
@@ -71,6 +83,35 @@
           } 
         });
       }, options);
+
+    let technologyItemTimeout = (item, time) => {
+      setTimeout(function() {
+        item.classList.remove('top-20', 'no-opacity');
+      }, 200 * time);
+    };
+
+    let technologyObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            let children = entry.target.children;
+            for (var i = 0; i < children.length; i++) {
+              technologyItemTimeout(children[i], i);
+            }
+          } 
+        });
+      }, options2 
+    )
+
+    let titleObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if(entry.isIntersecting) {
+            entry.target.classList.remove('top-20', 'no-opacity');
+          }
+        });
+      }, options2
+    )
     
     setTimeout(function() {
       splashCont.classList.toggle('no-opacity');
@@ -88,6 +129,10 @@
         }, 1000)
       }, 300);
       observer.observe(mainContent);
+      technologyContainers.forEach(container => {       
+        technologyObserver.observe(container)
+      });
+      titleObserver.observe(technologiesH2);
     });
   }
 })();
